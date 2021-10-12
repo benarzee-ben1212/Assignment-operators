b6ad149fee0b3100eddea1380a3ccc71d0210ecc
bennu <benarzee1212@gmail.com>
2021-10-12 19:08:57 +0530

    new

---
+++ index.js
@@ -0,0 +1,28 @@
+var num1=10
+var num2=10
+var num3=10
+
+//Addition
+console.log(num1 + num2 + num3)
+
+//subtraction
+console.log(num1 - num2 - num3)
+
+//multiplication
+console.log(num1 * num2 * num3)
+
+//division
+console.log(num2 / num3)
+
+//modulus(remainder)
+console.log(num2 % num3)
+
+console.log(num1++) //here it prints the valu before increment //here real value is 11
+console.log(num1++) //here it prints the value after increment //here real value is 12  //post increment
+
+console.log(++num1)//pre increment
+
+console.log(num1--)
+console.log(num1--)
+console.log(--num1)
+
